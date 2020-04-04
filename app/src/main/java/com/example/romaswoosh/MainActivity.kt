291a3e5d4604324a8c527bac02a2b9ce06e10719
Romeu  Bonini Neto <romeuboninineto@iMac-de-Romeu.local>
2020-04-04 00:20:44 -0300

    criei a intencao do botao da primeira activity

--- app/src/main/java/com/example/romaswoosh/MainActivity.kt
+++ app/src/main/java/com/example/romaswoosh/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.romaswoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getStartedBtn.setOnClickListener {
+
+            val ligaIntent = Intent(this, LigaActivity::class.java)
+            startActivity(ligaIntent)
+
+        }
     }
 }
